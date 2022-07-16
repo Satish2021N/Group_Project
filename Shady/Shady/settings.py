23965477e1424a31d05a6ae33b2299617aef6bf0
Satish2021N <satishdevilliers17@gmail.com>
2022-07-16 21:12:37 +0545

    Changes Made

--- Shady/Shady/settings.py
+++ Shady/Shady/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Shady',
         'USER' :'postgres',
-        'PASSWORD' : '30090',
+        'PASSWORD' : '1234',
         'HOST':'localhost'
     }
 }
